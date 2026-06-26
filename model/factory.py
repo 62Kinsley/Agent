33c3c5b1a1ab7f68f982177fe0e8425350641396
Qinjian Su <su.qin@northeastern.edu>
2026-06-26 13:00:54 -0700

    update factory.py

--- model/factory.py
+++ model/factory.py
@@ -3,16 +3,32 @@ from typing import Optional
 from langchain_core.embeddings import Embeddings
 from langchain.chat_models.base import BaseChatModel
 from langchain_qwq import ChatQwen
+from langchain_openai import OpenAIEmbeddings
+from utils.config_handler import rag_config, chroma_config, prompt_config, agent_config
 
-llm = ChatQwen(
-    model="qwen-flash",
-    max_tokens=3_000,
-    timeout=None,
-    max_retries=2,
-    # other params...
-)
-
-class BaseModelFactory(ABC):
+class BaseModelFactory(ABC):#abstract base class
     @abstractmethod
     def generator(self) -> Optional[Embeddings | BaseChatModel]:
         pass
+
+class ChatModelFactory(BaseModelFactory):
+    def generator(self) -> Optional[Embeddings | BaseChatModel]:
+        return ChatQwen(
+                    model=rag_config["chat_model_name"],
+                    max_tokens=3_000,
+                    timeout=None,
+                    max_retries=2,
+                )
+    
+class EmbeddingModelFactory(BaseModelFactory):
+    def generator(self) -> Optional[Embeddings | BaseChatModel]:
+        return OpenAIEmbeddings(
+                    model=rag_config["embedding_model_name"]
+                    base_url=rag_config["base_url"],
+                    api_key=rag_config["api_key"],
+                )
+    
+
+
+chat_model = ChatModelFactory().generator()
+embedding_model = EmbeddingModelFactory().generator()
