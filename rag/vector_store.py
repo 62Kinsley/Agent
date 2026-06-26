9233ccdaef1b76bae36f750f1091b460abfad8c5
Qinjian Su <su.qin@northeastern.edu>
2026-06-26 01:21:31 -0700

    add VectorStoreService

--- rag/vector_store.py
+++ rag/vector_store.py
@@ -1,1 +1,13 @@
-class 
+from langchain.chroma import Chroma
+from utils.config_handler import chroma_config
+
+class VectorStoreService:
+    def __init__(self):
+        self.vector_store = Chroma(
+            collection_name=chroma_config["collection_name"],
+            embedding_function=embeddings,
+            persist_directory=chroma_config["persist_directory"],
+        )
+        self.spliter = None
+
+   
